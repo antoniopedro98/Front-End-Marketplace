9d2dcddfa4d7c049b4a2b3a691d28e980c397041
antoniopedro98 <antoniopedrosantosalves@gmail.com>
2020-04-01 21:22:51 -0300

    Commit Inicial Front End Boca Boca

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { AnunciosCidadeComponent } from './anuncios-cidade/anuncios-cidade.component';
+import { AnuncioComponent } from './anuncio/anuncio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    AnunciosCidadeComponent,
+    AnuncioComponent
   ],
   imports: [
     BrowserModule,
